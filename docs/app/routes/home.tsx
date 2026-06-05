ee3926e9ea20944efe7d274536f1732958840414
Flux3tor <tusharsinha199@gmail.com>
2026-06-05 13:03:22 +0530

    added Sidebar Hotkeys

--- docs/app/routes/home.tsx
+++ docs/app/routes/home.tsx
@@ -500,6 +500,34 @@ export default function Home() {
               />
             </div>
           </div>
+
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-4 text-center items-center">
+              <h3 className="text-3xl font-medium">
+                Sidebar Hotkeys
+              </h3>
+
+              <p className="text-neutral-400 max-w-2xl leading-7">
+                Instantly navigate Stardance using customizable keyboard shortcuts and optional sidebar key hints.
+              </p>
+            </div>
+
+            <div className="rounded-2xl border border-neutral-800 overflow-hidden max-w-3xl mx-auto">
+             <img
+              src="/plugins/sidebar-hotkeys.png"
+              alt="Sidebar Hotkeys"
+              className="w-full"
+            />
+            </div>
+
+            <div className="rounded-2xl border border-neutral-800 overflow-hidden max-w-5xl mx-auto">
+              <img
+                src="/plugins/sidebar-hotkeys-demo.gif"
+                alt="Sidebar Hotkeys Demo"
+                className="w-full"
+              />
+            </div>
+          </div>
         </motion.div>
       </div>
 
